2e0bbefbef1d8a09cbb17dd0d7f220362815d6c6
kimjoonbhs <ekimthr@gmail.com>
2020-12-11 17:27:35 -0600

    Create XBoxInput.java

---
+++ FRC-2021-519eb99117bca5af38712bfea546d4edae352efa/src/main/java/frc/input/XBoxInput.java
@@ -0,0 +1,12 @@
+package frc.input;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+
+/**
+ * The input from the XBox remote
+ */
+public class XBoxInput extends InputMethod {
+  private XboxController controller;
+
+}
